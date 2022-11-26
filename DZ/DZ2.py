eb60c80492b0e88657c5b4281b556721301292e3
Vitalii <gilbertus722@yandex.ru>
2022-11-26 15:32:36 +0300

    задание 2 вер 3.2

--- DZ/DZ2.py
+++ DZ/DZ2.py
@@ -1,24 +1,8 @@
 """ Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.  """
 
 
-def inputNumbers(x):
-    xyz = ["X", "Y", "Z"]
-    a = []
-    for i in range(x):
-        a.append(input(f"Введите значение оси {xyz[i]}: "))
-    return a
-
-
-def checkPredicate(x):
-    left = not (x[0] or x[1] or x[2])
-    right = not x[0] and not x[1] and not x[2]
-    result = left == right
-    return result
-
-
-statement = inputNumbers(3)
-
-if checkPredicate(statement) == True:
-    print(f"Утверждение истинно")
-else:
-    print(f"Утверждение ложно")
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if not(x or y or z) == (not x and not y and not z):
+                print(f'Утверждение верно при значении переменных :\n x = {x}, y = {y}, z = {z}')
